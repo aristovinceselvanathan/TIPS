5f07925e583242483ceb64ad1fb0bdb17318c1ed
Aristo Vince <141934584+aristovinceselvanathan@users.noreply.github.com>
2023-08-15 10:19:13 +0530

    fix:variable,comments,switch case,removed files are added and input mismatch fixed

---
+++ src/Assignment1/MathUtils.cs
@@ -0,0 +1,52 @@
+﻿namespace Assignment1
+{
+    /// <summary>
+    /// This is a Mathematical Calculator Class
+    /// </summary>
+    public class MathUtils
+    {
+        /// <summary>
+        /// This is addition function
+        /// </summary>
+        /// <param name="input1">Operand 1</param>
+        /// <param name="input2">Operand 2</param>
+        /// <returns>It returns the addition of two operands</returns>
+        public int Add(int input1, int input2)
+        {
+            return input1 + input2;
+        }
+
+        /// <summary>
+        /// This is Subtraction function
+        /// </summary>
+        /// <param name="input1">Operand 1</param>
+        /// <param name="input2">Operand 2</param>
+        /// <returns>It returns the subtraction of two operands</returns>
+        public int Subtract(int input1, int input2)
+        {
+            return input1 - input2;
+        }
+
+        /// <summary>
+        /// This is Multiplication function
+        /// </summary>
+        /// <param name="input1">Operand 1</param>
+        /// <param name="input2">Operand 2</param>
+        /// <returns>It returns the multiplication of two operands</returns>
+        public int Multiplinput2(int input1, int input2)
+        {
+            return input1 * input2;
+        }
+
+        /// <summary>
+        /// This is Division Function
+        /// </summary>
+        /// <param name="input1">Operand 1</param>
+        /// <param name="input2">Operand 2</param>
+        /// <returns>It return the division of two operands</returns>
+        public float Divide(int input1, int input2)
+        {
+            return (float)input1 / (float)input2;
+        }
+    }
+}
--- src/Assignment1/Program.cs
+++ src/Assignment1/Program.cs
@@ -1,92 +1,132 @@
-﻿using System;
-
-namespace Assignment1
+﻿namespace Assignment1
 {
+    using System.Text.RegularExpressions;
+
     /// <summary>
     /// This is a Main Class Program
     /// </summary>
     public class Program
     {
+        private static bool flag1 = true;
+
         /// <summary>
         /// This is a Main Method
         /// </summary>
-        /// <param name="args"> This is a sample args</param>
+        /// <param name="args"> It is String array in the parameters of the main method</param>
         public static void Main(string[] args)
         {
-            MathUtils cal = new MathUtils();
-            Console.WriteLine("Enter the two numbers");
-            Console.Write("First Number : ");
-            int input1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Second Number: ");
-            int input2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter the options (1.Add, 2.Subtract, 3.Multiply, 4.Division, 5.Exit: ");
-            int option = Convert.ToInt32(Console.ReadLine());
-            if (option == 1)
+            int input1, input2, option;
+            bool flag = true;
+            while (flag)
             {
-                Console.WriteLine(cal.Add(input1, input2));
-            }
-            else if (option == 2)
-            {
-                Console.WriteLine(cal.Subtract(input1, input2));
-            }
-            else if (option == 3)
-            {
-                Console.WriteLine(cal.Multiply(input1, input2));
-            }
-            else if (option == 4)
-            {
-                Console.WriteLine(cal.Divide(input1, input2));
+                MathUtils calculator = new MathUtils();
+            label2:
+                Console.WriteLine("Enter the two numbers");
+                Console.Write("First Number : ");
+                input1 = IntegerInput(NullException(Console.ReadLine()));
+                if (!flag1)
+                {
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    flag1 = true;
+                    goto label2;
+                }
+
+                Console.Write("Second Number: ");
+                input2 = IntegerInput(NullException(Console.ReadLine()));
+                if (!flag1)
+                {
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                    flag1 = true;
+                    goto label2;
+                }
+
+            label:
+                Console.Write("Enter the options (1.Add, 2.Subtract, 3.Multiply, 4.Division, 5.Exit): ");
+                option = IntegerInput(NullException(Console.ReadLine()));
+                if (option <= 0 || option >= 6)
+                {
+                    goto label;
+                }
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine(calculator.Add(input1, input2));
+                        break;
+                    case 2:
+                        Console.WriteLine(calculator.Subtract(input1, input2));
+                        break;
+                    case 3:
+                        Console.WriteLine(calculator.Multiplinput2(input1, input2));
+                        break;
+                    case 4:
+                        Console.WriteLine(calculator.Divide(input1, input2));
+                        break;
+                    default:
+                        flag = false;
+                        break;
+                }
+
+                if (flag)
+                {
+                    Thread.Sleep(3000);
+                }
+                else
+                {
+                    Console.WriteLine("Exiting....");
+                    Thread.Sleep(1000);
+                }
+
+                Console.Clear();
             }
         }
-    }
 
-    /// <summary>
-    /// This is a Mathematical Calution Class
-    /// </summary>
-    public class MathUtils
-    {
         /// <summary>
-        /// This is addtion fuction
+        /// This Method checks for the Input Mismatch for string
         /// </summary>
-        /// <param name="x">Operand 1</param>
-        /// <param name="y">Operand 2</param>
-        /// <returns>It returns the addition of two operands</returns>
-        public int Add(int x, int y)
+        /// <param name="s">It Takes string as input</param>
+        /// <returns>It returns the integer</returns>
+        public static int IntegerInput(string s)
         {
-            return x + y;
+            try
+            {
+                Regex regex = new Regex("[0-9]{1}");
+                if (regex.IsMatch(s))
+                {
+                    return Convert.ToInt32(s);
+                }
+                else
+                {
+                    flag1 = false;
+                    Console.WriteLine("Invalid number");
+                    return 0;
+                }
+            }
+            catch (Exception)
+            {
+                flag1 = false;
+                Console.WriteLine("Invalid number");
+                return 0;
+            }
         }
 
         /// <summary>
-        /// This is Subtraction fuction
+        /// This is check it null
         /// </summary>
-        /// <param name="x">Operand 1</param>
-        /// <param name="y">Operand 2</param>
-        /// <returns>It returns the subtraction of two operands</returns>
-        public int Subtract(int x, int y)
+        /// <param name="s">It takes the string</param>
+        /// <returns>it returns string</returns>
+        public static string NullException(string? s)
         {
-            return x - y;
-        }
-
-        /// <summary>
-        /// This is Multiplication fuction
-        /// </summary>
-        /// <param name="x">Operand 1</param>
-        /// <param name="y">Operand 2</param>
-        /// <returns>It retruns the multiplication of two operands</returns>
-        public int Multiply(int x, int y)
-        {
-            return x * y;
-        }
-
-        /// <summary>
-        /// This is Division Function
-        /// </summary>
-        /// <param name="x">Operand 1</param>
-        /// <param name="y">Operand 2</param>
-        /// <returns>It return the division of two operands</returns>
-        public float Divide(int x, int y)
-        {
-            return (float)((float)x / (float)y);
+            if (string.IsNullOrEmpty(s))
+            {
+                return " ";
+            }
+            else
+            {
+                return s;
+            }
         }
     }
 }
