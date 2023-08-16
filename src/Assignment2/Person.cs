47d2d167d89ccb71a6381bcea207999f225b3b63
Aristo Vince <141934584+aristovinceselvanathan@users.noreply.github.com>
2023-08-16 22:59:55 +0530

    feat: Contacts Manager Console Application Added

---
+++ src/Assignment2/Person.cs
@@ -0,0 +1,161 @@
+﻿namespace Assignment2
+{
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// This class describes the person characteristics in contact manager
+    /// </summary>
+    public class Person
+    {
+        private string? Name { get; set; }
+
+        private string? Phone { get; set; }
+
+        private string? Email { get; set; }
+
+        private string? AdditionalNotes { get; set; }
+
+        /// <summary>
+        /// This is checks for null Exception of the string
+        /// </summary>
+        /// <param name="s">This gets the input of string type</param>
+        /// <returns>It returns string</returns>
+        public static string NullException(string? s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return " ";
+            }
+            else
+            {
+                return s;
+            }
+        }
+
+        /// <summary>
+        /// This method will get the details of the person
+        /// </summary>
+        /// <returns>It return the string of the details of the person</returns>
+        public string GetDetails()
+        {
+            return $"Name: {this.Name}\nPhone no: {this.Phone}\nEmail: {this.Email}\nAdditional Notes: {this.AdditionalNotes}";
+        }
+
+        /// <summary>
+        /// This method is to set the name
+        /// </summary>
+        /// <param name="name">This method will take the name as a string to set the name</param>
+        public void SetName(string name)
+        {
+            this.Name = name;
+        }
+
+        /// <summary>
+        /// This method get the name of person class
+        /// </summary>
+        /// <returns>It returns string</returns>
+        public string GetName()
+        {
+            return NullException(this.Name);
+        }
+
+        /// <summary>
+        /// This method returns the Email
+        /// </summary>
+        /// <returns>It returns string</returns>
+        public string GetPhone()
+        {
+            return NullException(this.Phone);
+        }
+
+        /// <summary>
+        /// This method returns the phone number
+        /// </summary>
+        /// <returns>It returns string</returns>
+        public string GetEmail()
+        {
+            return NullException(this.Email);
+        }
+
+        /// <summary>
+        /// This method is to set the phone number
+        /// </summary>
+        /// <param name="phone">This method will take the name as a string to set the phone</param>
+        /// <returns>It returns the boolean</returns>
+        public bool SetPhone(string phone)
+        {
+            Regex regex = new Regex("[0-9]{10}");
+            if (regex.IsMatch(phone))
+            {
+                this.Phone = phone;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Phone number");
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// This method is to set the email
+        /// </summary>
+        /// <param name="email">This method will take the name as a string to set the email</param>
+        /// <returns>It returns the Boolean type</returns>
+        public bool SetEmail(string email)
+        {
+            Regex regex = new Regex("^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,}$");
+            if (regex.IsMatch(email))
+            {
+                this.Email = email;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Email");
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// This method is to set the notes
+        /// </summary>
+        /// <param name="notes">This method will take the name as a string to set the Additional Notes</param>
+        public void SetNotes(string notes)
+        {
+            this.AdditionalNotes = notes;
+        }
+
+        /// <summary>
+        /// This method overrides the equals method in object class
+        /// </summary>
+        /// <param name="obj"> It uses the instance of the object class</param>
+        /// <returns>It returns bool</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            Person? other = obj as Person;
+            if (other == null)
+            {
+                return false;
+            }
+
+            return this.Phone == other.Phone;
+        }
+
+        /// <summary>
+        /// It overrides the GetHashCode when Equals Method overridden in the person class
+        /// </summary>
+        /// <returns>It returns the Integer</returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
