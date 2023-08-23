4afdca085d37c4ef5a39fe493487c09e43ebb323
Aristo Vince <141934584+aristovinceselvanathan@users.noreply.github.com>
2023-08-23 10:13:13 +0530

    feat : Add the print functionality

---
+++ Git/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Git
+{
+    /// <summary>
+    /// Program Class
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main method that prints Hello World
+        /// </summary>
+        /// <param name="args">It takes the argument from the command line interface</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World..");
+            Console.WriteLine("Practice Git in Visual Studio");
+        }
+    }
+}
