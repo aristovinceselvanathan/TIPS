a47c2e6b67b29891e11e82ed3a057f135ceb4f8f
Aristo Vince <141934584+aristovinceselvanathan@users.noreply.github.com>
2023-08-25 18:07:56 +0530

    fix : changes in readme file

---
+++ src/Assignment3/Account.cs
@@ -0,0 +1,39 @@
+﻿namespace Assignment3
+{/// <summary>
+ /// Account Class
+ /// </summary>
+    public class Account
+    {
+        /// <summary>
+        /// Gets or Sets the string
+        /// </summary>
+        /// <value>
+        /// Email from main method
+        /// </value>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether active
+        /// </summary>
+        /// <value>
+        /// Active status
+        /// </value>
+        public bool Active { get; set; }
+
+        /// <summary>
+        /// Gets or Sets the DateTime
+        /// </summary>
+        /// <value>
+        /// CreatedDate
+        /// </value>
+        public DateTime CreatedDate { get; set; }
+
+        /// <summary>
+        /// Gets or Sets the Ilist that contains Roles
+        /// </summary>
+        /// <value>
+        /// Roles
+        /// </value>
+        public IList<string> Roles { get; set; }
+    }
+}
---
+++ src/Assignment3/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Assignment3
+{
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// TProgram Class
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main method that prints the list.
+        /// </summary>
+        /// <param name="args">It takes string array from the command line interface</param>
+        public static void Main(string[] args)
+        {
+            Account account = new Account
+            {
+                Email = "james@example.com",
+                Active = true,
+                CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
+                Roles = new List<string> { "User", "Admin" },
+            };
+            Console.WriteLine("Helloo");
+            string json = JsonConvert.SerializeObject(account, Newtonsoft.Json.Formatting.Indented);
+            Console.WriteLine(json);
+        }
+    }
+}
