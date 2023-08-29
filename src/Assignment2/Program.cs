53f9cefad1adacd551d5ee55177266a80ddcff24
Aristo Vince <141934584+aristovinceselvanathan@users.noreply.github.com>
2023-08-29 11:11:15 +0530

    fix : enum

--- src/Assignment2/Program.cs
+++ src/Assignment2/Program.cs
@@ -123,7 +123,7 @@
 
                             break;
                         case (int)Option.DisplayAll:
-                            h  DisplayAll(phoneDirectory);
+                            DisplayAll(phoneDirectory);
                             break;
                         case (int)Option.Exit:
                             flag = false;
