943c1daa488b3ce4af35040c374aa70a597f2ac2
Aristo Vince <141934584+aristovinceselvanathan@users.noreply.github.com>
2023-08-30 11:11:01 +0530

    fix : add assignment files

---
+++ src/Assignment8/ValueAndReferenceTypes/Employee.cs
@@ -0,0 +1,41 @@
+﻿namespace ValueAndReferenceTypes
+{
+    /// <summary>
+    /// Employee Class
+    /// </summary>
+    internal class Employee
+    {
+        /// <summary>
+        /// Gets or Sets Id
+        /// </summary>
+        /// <value>
+        /// String
+        /// </value>
+        public string Id { get; set;  }
+
+        /// <summary>
+        /// Gets or Sets Name
+        /// </summary>
+        /// <value>
+        /// String
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Id
+        /// </summary>
+        /// <value>
+        /// String
+        /// </value>
+        public string Designation{ get; set; }
+
+        /// <summary>
+        /// It overrides the ToString method
+        /// </summary>
+        /// <returns>It returns the string</returns>
+        public override string ToString()
+        {
+            return $"Employee Details: \nId : {this.Id} \nName : {this.Name} \nDesignation : {this.Designation}";
+        }
+    }
+}
