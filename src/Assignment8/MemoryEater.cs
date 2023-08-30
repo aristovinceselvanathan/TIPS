adaa9d696088152edefebd3dd2cc2c7edad0cd66
Aristo Vince <141934584+aristovinceselvanathan@users.noreply.github.com>
2023-08-30 11:54:22 +0530

    feat : files added

---
+++ src/Assignment8/MemoryEater.cs
@@ -0,0 +1,40 @@
+﻿namespace Assignment8
+{
+    /// <summary>
+    /// Memory Eater Class
+    /// </summary>
+    public class MemoryEater
+    {
+        private List<int[]> _memA110c = new List<int[]>();
+
+        /// <summary>
+        /// It will allocate the memory to the list
+        /// </summary>
+        public void Allocate()
+        {
+            while (true)
+            {
+                _memA110c.Add(new int[1000]);
+
+                // Assume memA110c variable is used only within this loop.
+                Thread.Sleep(10);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Program Class
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Main Method
+        /// </summary>
+        /// <param name="strings">It takes the string array from the command line interface</param>
+        public static void Main(string[] strings)
+        {
+            MemoryEater me = new MemoryEater();
+            me.Allocate();
+        }
+    }
+}
