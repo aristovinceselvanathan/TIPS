c5d7d8bf21335279e618ba4d8e806f799bfe3f99
Aristo Vince <141934584+aristovinceselvanathan@users.noreply.github.com>
2023-09-03 00:01:52 +0530

    feat : added files

---
+++ src/Assignment13/MSBuild/Calculator.cs
@@ -0,0 +1,17 @@
+﻿namespace MSBuild
+{
+    /// <summary>
+    /// Class Program
+    /// </summary>
+    internal class Calculator
+    {
+        /// <summary>
+        /// It is main method
+        /// </summary>
+        /// <param name="args">It takes the string array form the command line interface</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
