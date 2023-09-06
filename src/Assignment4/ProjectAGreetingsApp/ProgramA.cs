4bbb7fe4a759c953c95a3dabc50ba37cd6fa10bc
Aristo Vince <141934584+aristovinceselvanathan@users.noreply.github.com>
2023-09-06 11:44:09 +0530

    feat: assignment 4 circular dependencies (#5)
    * feat: assignment 4 circular dependencies projects files update
    
    * fix : made changes in the variables
    
    * fix : fix variable declaration
    
    * fix : added color in console application
    
    * fix : add enum
    
    * fix : add solution file
    
    * fix : remove main methods
    
    * fix : updates in ReadMe File
    
    * fix : changes in else logic
    
    * feat: assignment 4 circular dependencies projects files update
    
    * fix : made changes in the variables
    
    * fix : fix variable declaration
    
    * fix : add solution file
    
    * fix : added color in console application
    
    * fix : add enum
    
    * fix : remove main methods
    
    * fix : updates in ReadMe File
    
    * fix : changes in else logic
    
    * fix : modify sln file

---
+++ src/Assignment4/ProjectAGreetingsApp/ProgramA.cs
@@ -0,0 +1,65 @@
+﻿namespace ProjectAGreetingsApp
+{
+    using ProjectBMathApp;
+    using ProjectCDisplayApp;
+    using ProjectDUtilityApp;
+
+    /// <summary>
+    /// ProgramA Class
+    /// </summary>
+    internal class ProgramA
+    {
+        /// <summary>
+        /// Main method prints the greeting message of Hello World and It creates the object of B and pass it method of class C.
+        /// Main method starts by the asking the two inputs from the user and checks for the input mismatch.
+        /// Main method asks for the operations that are to be performed. The loop runs as long as user terminate by the keyword.
+        /// </summary>
+        /// <param name="args">It is string array that returns from the command line interface</param>
+        public static void Main(string[] args)
+        {
+            int inputOfOperand1, inputOfOperand2, option;
+            string isNumberOfOperand1, isNumberOfOperand2;
+            bool flag = true;
+            ProgramB mathOperation = new ProgramB();
+            ProgramC displayResults = new ProgramC();
+            ProgramD userInterface = new ProgramD();
+
+            displayResults.GetObjectOfProgramB(mathOperation);
+            while (flag)
+            {
+                Console.Write("Welcome to Calculator Application\nEnter the Input1 : ");
+                isNumberOfOperand1 = Console.ReadLine();
+                if (!int.TryParse(isNumberOfOperand1, out inputOfOperand1))
+                {
+                    userInterface.InvalidNumberWarning("Input 1");
+                    continue;
+                }
+
+                Console.Write("Enter the Input2 : ");
+                isNumberOfOperand2 = Console.ReadLine();
+                if (int.TryParse(isNumberOfOperand2, out inputOfOperand2))
+                {
+                    Console.Write("Enter the Option : 1.Addition 2.Subtraction 3.Multiplication 4.Division 5.Exit : ");
+                    if (int.TryParse(Console.ReadLine(), out option) || (option > 6 || option < 1))
+                    {
+                        flag = displayResults.Display(inputOfOperand1, inputOfOperand2, option);
+                    }
+                    else
+                    {
+                        userInterface.InvalidNumberWarning("Option");
+                        continue;
+                    }
+                }
+                else
+                {
+                    userInterface.InvalidNumberWarning("Input 2");
+                    continue;
+                }
+
+                Console.WriteLine("Press Enter to Continue :");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
