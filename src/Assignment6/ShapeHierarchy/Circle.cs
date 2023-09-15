493a49f96c60583800882950318982213b0047a8
Aristo Vince <141934584+aristovinceselvanathan@users.noreply.github.com>
2023-09-15 12:05:49 +0530

    Feat: assignment 6 oops concept (#6)
    * feat: assignment6 oops concept files added
    
    * fix : modify declaration of variables and methods
    
    * fix : modify .github file
    
    * fix : corner conditions are fix
    
    * fix : code complexity issues fix
    
    * fix : fix variable declaration
    
    * fix : fix solution file
    
    * fix : add color element
    
    * fix : chages the variable declarartion
    
    * fix : variable declaration
    
    * fix : chages in logic
    
    * fix : chages in path
    
    * fix : chages in file
    
    * fix : added enum
    
    * fix : chages in variable
    
    * feat: assignment6 oops concept files added
    
    * fix : modify declaration of variables and methods
    
    * fix : corner conditions are fix
    
    * fix : code complexity issues fix
    
    * fix : fix variable declaration
    
    * fix : fix solution file
    
    * fix : add color element
    
    * fix : chages the variable declarartion
    
    * fix : variable declaration
    
    * fix : chages in logic
    
    * fix : chages in path
    
    * fix : chages in file
    
    * fix : added enum
    
    * fix : chages in variable
    
    * fix : changes in sln file

---
+++ src/Assignment6/ShapeHierarchy/Circle.cs
@@ -0,0 +1,27 @@
+﻿namespace ShapeHierarchy
+{
+    /// <summary>
+    /// Circle Class
+    /// </summary>
+    internal class Circle : Shape
+    {
+        /// <summary>
+        /// It returns the area of circle by the formula of 𝞹*(r^2)
+        /// </summary>
+        /// <returns>It returns the double type of the answer from the above formula</returns>
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(this.Input1, 2);
+        }
+
+        /// <summary>
+        /// It prints the colour, area, name of the shape
+        /// </summary>
+        public override void PrintDetails()
+        {
+            Console.WriteLine($"Shape is {this.GetType().Name}");
+            Console.WriteLine($"Color is {this.Color}");
+            Console.WriteLine($"Area is {this.CalculateArea()} cm²");
+        }
+    }
+}
