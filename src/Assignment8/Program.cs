a75e92566d27171996eb83d59d6e9e172cd3b323
Aristo Vince <141934584+aristovinceselvanathan@users.noreply.github.com>
2023-09-25 18:06:14 +0530

    fix : changes in comments

--- src/Assignment8/Program.cs
+++ src/Assignment8/Program.cs
@@ -8,8 +8,8 @@
         /// <summary>
         /// Main Method it will create the object and call the allocate method in the MemoryEater Class
         /// </summary>
-        /// <param name="strings">It takes the string array from the command line interface</param>
-        public static void Main(string[] strings)
+        /// <param name="args">It is string array that returns from the command line interface</param>
+        public static void Main(string[] args)
         {
             MemoryEater me = new MemoryEater();
             me.Allocate();
