952505ff9fe8566e4ec282632ce75cbd80477dca
Aristo Vince <141934584+aristovinceselvanathan@users.noreply.github.com>
2023-10-14 17:39:09 +0530

    fix : chnges in exception file

--- src/Assignment11/ErrorHandling/InvalidUserInputException.cs
+++ src/Assignment11/ErrorHandling/InvalidUserInputException.cs
@@ -1,18 +1,18 @@
 ﻿namespace ErrorHandling
 {
     /// <summary>
-    /// It is exception class to handle the invalid user
+    /// Exception class to handle the invalid user
     /// </summary>
     internal class InvalidUserInputException : Exception
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="InvalidUserInputException"/> class.
         /// </summary>
-        /// <param name="message">User Entered Message to throw exception</param>
+        /// <param name="message">Message about the error to throw exception</param>
         public InvalidUserInputException(string message)
-            : base($"{message}")
+            : base(message)
         {
-            Console.ForegroundColor = ConsoleColor.Red; // It sets the color of the console text.
+            Console.ForegroundColor = ConsoleColor.Red;
         }
     }
 }
