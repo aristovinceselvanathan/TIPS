40f1ceeec33bd726b292f5dfe707b41f515e6125
Aristo Vince <141934584+aristovinceselvanathan@users.noreply.github.com>
2023-12-05 15:20:21 +0530

    fix : added fizzbuzz solution

---
+++ src/Phase2Task/Phase2Task/FizzBuzzMultiThreaded/FizzBuzz.cs
@@ -0,0 +1,110 @@
+﻿namespace FizzBuzzMultiThreaded
+{
+    /// <summary>
+    /// FizzBuzz Class
+    /// </summary>
+    internal class FizzBuzz
+    {
+        private int number;
+        private int currentInt = 1;
+        private object locker = new object();
+
+        /// <summary>
+        /// Sets the number value
+        /// </summary>
+        /// <param name="number">Number of the Iteration</param>
+        public FizzBuzz(int number)
+        {
+            this.number = number;
+        }
+
+        /// <summary>
+        /// Call the printFizz method when it is divisible by 3
+        /// </summary>
+        /// <param name="printFizz">Reference of the printFizz Method</param>
+        public void Fizz(Action printFizz)
+        {
+            while(currentInt <= number)
+            {
+                lock(locker)
+                {
+                    if(currentInt > number)
+                    {
+                        return;
+                    }
+                    if(currentInt % 3 == 0 && currentInt % 5 != 0) 
+                    { 
+                        printFizz();
+                        currentInt++;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Call the printBuzz method when it is divisible by 5
+        /// </summary>
+        /// <param name="printFizz">Reference of the printBuzz Method</param>
+        public void Buzz(Action printBuzz)
+        {
+            while(currentInt <= number)
+            {
+                lock(locker)
+                {
+                    if(currentInt > number)
+                    {
+                        return;
+                    }
+                    if(currentInt % 5 == 0 && currentInt % 3 != 0)
+                    {
+                        printBuzz();
+                        currentInt++;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Call the printFizz method when it is divisible by 3 and 5
+        /// </summary>
+        /// <param name="printFizz">Reference of the print Fizzbuzz Method</param>
+        public void Fizzbuzz(Action printFizzBuzz)
+        {
+            while (currentInt <= number)
+            {
+                lock (locker)
+                {
+                    if (currentInt > number)
+                    {
+                        return;
+                    }
+                    if (currentInt % 5 == 0 && currentInt % 3 == 0)
+                    {
+                        printFizzBuzz();
+                        currentInt++;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Call the printFizz method when it is not divisible by 3 and 5
+        /// </summary>
+        /// <param name="printFizz">Reference of the printNumber Method</param>
+        public void Number(Action<int> printNumber)
+        {
+            while (currentInt <= number)
+            {
+                lock (locker)
+                {
+                    if (currentInt > number)
+                    {
+                        return;
+                    }
+                    if (currentInt % 5 != 0 && currentInt % 3 != 0)
+                    {
+                        printNumber(currentInt);
+                        currentInt++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/Phase2Task/Phase2Task/FizzBuzzMultiThreaded/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace FizzBuzzMultiThreaded
+{
+    /// <summary>
+    /// Program Class
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// It creates the thread for each methods in the fizzBuzz class
+        /// </summary>
+        /// <param name="args">It is string array in the parameters of the main method</param>
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the N Value : ");
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                Console.Write("[");
+                FizzBuzz fizzBuzz = new FizzBuzz(number);
+                List<Thread> threads = new List<Thread>() {
+                new Thread(x => fizzBuzz.Fizz(printFizz)),
+                new Thread(x => fizzBuzz.Buzz(printBuzz)),
+                new Thread(x => fizzBuzz.Fizzbuzz(printFizzBuzz)),
+                new Thread(x => fizzBuzz.Number(printNumber)),
+                };
+
+                threads.ForEach(x => x.Start());
+                threads.ForEach(x => x.Join());
+                Console.Write("]");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input Value");
+            }
+        }
+        /// <summary>
+        /// To Print Fizz in the Console
+        /// </summary>
+        public static void printFizz()
+        {
+            Console.Write("Fizz, ");
+        }
+        /// <summary>
+        /// To Print Buzz in the Console
+        /// </summary>
+        public static void printBuzz()
+        {
+            Console.Write("Buzz, ");
+        }
+        /// <summary>
+        /// To Print FizzBuzz in the Console
+        /// </summary>
+        public static void printFizzBuzz()
+        {
+            Console.Write("FizzBuzz, ");
+        }
+        /// <summary>
+        /// To Print Number in the Console
+        /// </summary>
+        public static void printNumber(int number)
+        {
+            Console.Write($"{number}, ");
+        }
+    }
+}
